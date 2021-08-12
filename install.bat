48328f6864fa16d431298357d5e0f2a7a7fd0f2c
Kelps Sousa Alux <code@kelps.net>
2021-08-12 15:09:32 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,25 +4,25 @@ choco install 7zip -y
 choco install kdiff3 -y
 choco install filezilla -y
 choco install fiddler -y
-@REM choco install postman -y
-choco install soapui -y
-choco install webpi -y
+choco install postman -y
+@REM choco install soapui -y
+@REM choco install webpi -y
 
 @REM MVC 3
-choco install aspnetmvc.install -y
+@REM choco install aspnetmvc.install -y
 choco install googlechrome -y
-choco install firefox -y
-choco install opera -y
+@REM choco install firefox -y
+@REM choco install opera -y
 choco install -y linkshellextension
-choco install blender -y
-choco install cura -y
-choco install audacity -y
+@REM choco install blender -y
+@REM choco install cura -y
+@REM choco install audacity -y
 
 @echo Openning apps directly in the Windows Store app
 @REM https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-store-app 
 
-start "" ms-windows-store://pdp/?ProductId=9N2T6F9F5ZDN @REM Nightingale REST Client
-@pause
+@REM start "" ms-windows-store://pdp/?ProductId=9N2T6F9F5ZDN @REM Nightingale REST Client
+@REM @pause
 start "" ms-windows-store://pdp/?ProductId=9NBLGGH4VZW5 @REM Autodesk Sketchbook
 @pause
 start "" ms-windows-store://pdp/?ProductId=9WZDNCRFJ262 @REM Nextgen Reader
@@ -31,14 +31,14 @@ start "" ms-windows-store://pdp/?ProductId=9NBLGGGZNGR9 @REM Trakter
 @pause
 start "" ms-windows-store://pdp/?ProductId=9NJ3KMH29VGJ @REM Enpass Password Manager
 @pause
-start "" ms-windows-store://pdp/?ProductId=9NCBCSZSJRSB @REM Spotify
-@pause
+@REM start "" ms-windows-store://pdp/?ProductId=9NCBCSZSJRSB @REM Spotify
+@REM @pause
 start "" ms-windows-store://pdp/?ProductId=9NBHCS1LX4R0 @REM Paint.NET
 @pause
-start "" ms-windows-store://pdp/?ProductId=9NBLGGH5279M @REM Open Live Writer
-@pause
-start "" ms-windows-store://pdp/?ProductId=9WZDNCRFJ3Q8 @REM Plex
-@pause
+@REM start "" ms-windows-store://pdp/?ProductId=9NBLGGH5279M @REM Open Live Writer
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9WZDNCRFJ3Q8 @REM Plex
+@REM @pause
 start "" ms-windows-store://pdp/?ProductId=9NKSQGP7F2NH @REM WhatsApp desktop
 @pause
 start "" ms-windows-store://pdp/?ProductId=9WZDNCRDMDM3 @REM Nuget Package Explorer
@@ -49,23 +49,25 @@ start "" ms-windows-store://pdp/?ProductId=9NBLGGH35LRM @REM Affinity Designer
 start "" ms-windows-store://pdp/?ProductId=9NBLGGH35LXN @REM Affinity Photo
 @pause
 
-start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K5DM @REM Office 365 Home
-@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K5F3 @REM Excel 2016
-@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K5D7 @REM Word 2016
-@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K5CT @REM PowerPoint 2016
-@pause
+@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K5DM @REM Office 365 Home
+@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K7KX @REM Excel
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K7C7 @REM Word 2016
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=CFQ7TTC0K7C6 @REM PowerPoint 2016
+@REM @pause
 
-start "" ms-windows-store://pdp/?ProductId=9NBLGGH4R9NZ @REM AdBlock Plus
-@pause
-start "" ms-windows-store://pdp/?ProductId=9NZ9D2J86W6S @REM JSON Formatter for Edge
-@pause
-start "" ms-windows-store://pdp/?ProductId=9NBLGGH4QWS7 @REM Page Analyzer
-@pause
-start "" ms-windows-store://pdp/?ProductId=9PBGQPBGBFH5 @REM Web Developer Checklist
-@pause
-start "" ms-windows-store://pdp/?ProductId=9N8H1T7MZC7W @REM BrowserStack
-@pause
-start "" ms-windows-store://pdp/?ProductId=9MSPC6MP8FM4 @REM Microsoft Whiteboard
+@REM start "" ms-windows-store://pdp/?ProductId=9NBLGGH4R9NZ @REM AdBlock Plus
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9NZ9D2J86W6S @REM JSON Formatter for Edge
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9NBLGGH4QWS7 @REM Page Analyzer
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9PBGQPBGBFH5 @REM Web Developer Checklist
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9N8H1T7MZC7W @REM BrowserStack
+@REM @pause
+@REM start "" ms-windows-store://pdp/?ProductId=9MSPC6MP8FM4 @REM Microsoft Whiteboard
 
 start "" https://www.microsoft.com/en-us/sql-server/sql-server-editions-express @REM SQL Express
 @REM look into https://chocolatey.org/packages/sql-server-express
